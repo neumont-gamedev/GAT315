44f3bf28c23ad264fb658fdf5d7c5a73ebac787a
neumont-gamedev <rmaple@neumont.edu>
2021-05-24 18:30:07 -0600

    class 052421

--- Assets/Scripts/Kinematics/Coordinate.cs
+++ Assets/Scripts/Kinematics/Coordinate.cs
@@ -12,10 +12,10 @@ public static class Coordinate
 
 	public static Vector2 PolarToCartesian(Polar polar)
 	{
-		Vector2 v2 = Vector2.zero;
-		v2.x = Mathf.Cos(polar.angle * Mathf.Deg2Rad) * polar.length;
-		v2.y = Mathf.Sin(polar.angle * Mathf.Deg2Rad) * polar.length;
-		return v2;
+		Vector2 v = Vector2.zero;
+		v.x = Mathf.Cos(polar.angle * Mathf.Deg2Rad) * polar.length;
+		v.y = Mathf.Sin(polar.angle * Mathf.Deg2Rad) * polar.length;
+		return v;
 	}
 
 	public static Polar CartesianToPolar(Vector2 v)
