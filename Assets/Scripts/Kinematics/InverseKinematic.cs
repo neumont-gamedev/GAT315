982ce01c22497afdc4038f5cf926df156edb2ad2
neumont-gamedev <rmaple@neumont.edu>
2021-05-26 13:50:24 -0600

    water shader graph

--- Assets/Scripts/Kinematics/InverseKinematic.cs
+++ Assets/Scripts/Kinematics/InverseKinematic.cs
@@ -8,8 +8,9 @@ public class InverseKinematic : MonoBehaviour
     public int count = 5;
     [Range(0.1f, 3.0f)] public float size = 1;
     [Range(0.1f, 3.0f)] public float length = 1;
-    public Transform target;
 
+    public Transform target;
+    public Transform anchor;
 
     List<InverseKinematicSegment> segments = new List<InverseKinematicSegment>();
 
@@ -36,6 +37,17 @@ public class InverseKinematic : MonoBehaviour
             Vector2 position = (segment.parent) ? segment.parent.start : (Vector2)target.position;
             segment.Follow(position);
         }
+
+        if (anchor)
+		{
+            int base_index = segments.Count - 1;
+            segments[base_index].start = anchor.position;
+
+            for (int i = base_index - 1; i >= 0; i--)
+			{
+                segments[i].start = segments[i + 1].end;
+			}
+		}
     }
 
 
