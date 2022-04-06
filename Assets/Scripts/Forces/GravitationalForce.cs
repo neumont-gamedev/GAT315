e561b60802ea116f544f419329d38497846f958f
neumont-gamedev <rmaple@neumont.edu>
2022-04-05 21:01:48 -0600

    preclass 040622
    gravitational force

--- Assets/Scripts/Forces/GravitationalForce.cs
+++ Assets/Scripts/Forces/GravitationalForce.cs
@@ -8,6 +8,22 @@ public class GravitationalForce : Force
 
 	public override void ApplyForce(List<Body> bodies)
 	{
-		//throw new System.NotImplementedException();
+		{
+			for (int i = 0; i < bodies.Count - 1; i++)
+			{
+				for (int j = i + 1; j < bodies.Count; j++)
+				{
+					Body bodyA = bodies[i];
+					Body bodyB = bodies[j];
+
+					Vector2 direction = bodyA.position - bodyB.position;
+					float distanceSqr = Mathf.Max(direction.sqrMagnitude, 1);
+					float force = gravitation.value * ((bodyA.mass * bodyB.mass) / distanceSqr);
+
+					bodyA.ApplyForce(-direction.normalized * force, Body.eForceMode.Force);
+					bodyB.ApplyForce( direction.normalized * force, Body.eForceMode.Force);
+				}
+			}
+		}
 	}
 }
