6fc3a8191dad143034d09f04b221d5f413799aa7
neumont-gamedev <rmaple@neumont.edu>
2022-04-07 18:55:32 -0600

    preclass 040722

--- Assets/Scripts/Physics/Simulator.cs
+++ Assets/Scripts/Physics/Simulator.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class Simulator : Singleton<Simulator>
 {
+	[SerializeField] IntData fixedFPS;
+	[SerializeField] StringData fps;
 	[SerializeField] List<Force> forces;
 
 	public List<Body> bodies { get; set; } = new List<Body>();
+	public float fixedDeltaTime => 1.0f / fixedFPS.value;
+
 	Camera activeCamera;
+	float timeAccumulator = 0;
 
 	private void Start()
 	{
@@ -16,15 +21,27 @@ public class Simulator : Singleton<Simulator>
 
 	private void Update()
 	{
+		// get fps
+		fps.value = (1.0f / Time.deltaTime).ToString("F2");
+
+		// add current delta time to time accumulator
+		timeAccumulator += Time.deltaTime;
+
+		// apply forces to bodies
 		forces.ForEach(force => force.ApplyForce(bodies));
 
-		bodies.ForEach(body =>
+		// integrate physics simulation with fixed delta time
+		while (timeAccumulator >= fixedDeltaTime)
 		{
-			Integrator.SemiImplicitEuler(body, Time.deltaTime);
-		});
+			bodies.ForEach(body =>
+			{
+				Integrator.SemiImplicitEuler(body, fixedDeltaTime);
+			});
+			timeAccumulator -= fixedDeltaTime;
+		}
 
+		// reset body acceleration
 		bodies.ForEach(body => body.acceleration = Vector2.zero);
-
 	}
 
 	public Vector3 GetScreenToWorldPosition(Vector2 screen)
