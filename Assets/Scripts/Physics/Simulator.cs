76e52e4584c1a3d19a8f1cd90a4277af9aca9963
neumont-gamedev <rmaple@neumont.edu>
2022-04-14 16:14:27 -0600

    class 041322

--- Assets/Scripts/Physics/Simulator.cs
+++ Assets/Scripts/Physics/Simulator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -42,6 +43,20 @@ public class Simulator : Singleton<Simulator>
 
 		// reset body acceleration
 		bodies.ForEach(body => body.acceleration = Vector2.zero);
+	}
+
+	public Body GetScreenToBody(Vector3 screen)
+	{
+		Body body = null;
+
+		Ray ray = activeCamera.ScreenPointToRay(screen);
+		RaycastHit2D hit = Physics2D.GetRayIntersection(ray);
+		if (hit.collider)
+		{
+			hit.collider.gameObject.TryGetComponent<Body>(out body);
+		}
+
+		return body;
 	}
 
 	public Vector3 GetScreenToWorldPosition(Vector2 screen)
